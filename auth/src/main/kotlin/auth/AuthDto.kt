581a624c31cba9ce6e97e5e857f11f614e5ab3cf
bragekankode <KnulleGutta1997>
2020-12-06 18:12:02 +0100

    added auth added MOMListener.kt

---
+++ auth/src/main/kotlin/auth/AuthDto.kt
@@ -0,0 +1,12 @@
+package auth
+
+import javax.validation.constraints.NotBlank
+
+class AuthDto(
+        @get:NotBlank
+        var userId : String? = null,
+
+        @get:NotBlank
+        var password: String? = null
+)
+
---
+++ movies/src/main/kotlin/movies/MOMListener.kt
@@ -0,0 +1,26 @@
+package movies
+
+import movies.db.MovieStatsService
+import org.slf4j.LoggerFactory
+import org.springframework.amqp.rabbit.annotation.RabbitListener
+import org.springframework.stereotype.Service
+
+@Service
+class MOMListener(
+        private val statsService: MovieStatsService
+) {
+
+    companion object{
+        private val log = LoggerFactory.getLogger(MOMListener::class.java)
+    }
+
+
+    @RabbitListener(queues = ["#{queue.name}"])
+    fun receiveFromAMQP(userId: String) {
+        val ok = statsService.registerNewUser(userId)
+        if(ok){
+            log.info("Registered new user via MOM: $userId")
+        }
+    }
+}
+
